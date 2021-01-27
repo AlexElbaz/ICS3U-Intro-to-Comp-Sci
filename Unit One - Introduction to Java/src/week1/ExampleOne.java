5741dc37e35e232b2e06b73c68cdf431f346a328
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-01-27 10:04:18 -0500

    Println and Escape Sequences

--- Unit One - Introduction to Java/src/week1/ExampleOne.java
+++ Unit One - Introduction to Java/src/week1/ExampleOne.java
@@ -1,11 +1,13 @@
 package week1;
-
-/**
- * ExampleOne
- */
+/*
+* ExampleOne is the name of the Class. It has the same name as the file.
+* Classes are meant to model an object's behaviour.
+* Naming convention for Classes is upper case on the first letter of each word (no spaces).
+*/ 
 public class ExampleOne {
-
     public static void main(String[] args) {
-        System.out.println("Welcome to ICS3U...");
+        System.out.println("Welcome to ICS3U..."); // println writes message to the console then adds new line
+        System.out.print("This is another line that does not end with a new line."); // print simply writes a message to the console, no new line is added afterwards
+        System.out.println("This is another line!");
     }
 }
---
+++ Unit One - Introduction to Java/src/week1/ExampleTwo.java
@@ -0,0 +1,7 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        
+    }
+}

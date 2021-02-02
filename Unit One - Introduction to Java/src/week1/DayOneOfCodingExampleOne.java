6ca9e3c5fad1f30938554cd33240b196213b7999
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-02-02 08:46:34 -0500

    Remaning of Files to make them easier to navigate and updating DayOneOfCodingExampleTwo

--- Unit One - Introduction to Java/src/week1/DayOneOfCodingExampleOne.java
+++ Unit One - Introduction to Java/src/week1/DayOneOfCodingExampleOne.java
@@ -4,7 +4,7 @@ package week1;
 * Classes are meant to model an object's behaviour.
 * Naming convention for Classes is upper case on the first letter of each word (no spaces).
 */ 
-public class ExampleOne {
+public class DayOneOfCodingExampleOne {
     public static void main(String[] args) {
         System.out.println("Welcome to ICS3U..."); // println writes message to the console then adds new line
         System.out.print("This is another line that does not end with a new line."); // print simply writes a message to the console, no new line is added afterwards

279ea6770e9d689038962744a91e97e061e07a29
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-02-02 11:12:02 -0500

    Using println and beginning escape sequences; learning what characters can be escaped and what they do.

--- Unit One - Introduction to Java/src/week1/DayOneOfCodingExampleTwo.java
+++ Unit One - Introduction to Java/src/week1/DayOneOfCodingExampleTwo.java
@@ -1,15 +1,12 @@
 package week1;
 
 public class DayOneOfCodingExampleTwo {
-        public static void main(String[] args) {
-          System.out.println("This is very \"exciting\" code!");    // "" identify the beginning and end of String literals - text you want to include in the code.
+    public static void main(String[] args) {
+        System.out.println("This is very \"exciting\" code!");    // "" identify the beginning and end of String literals - text you want to include in the code.
                                                                     // backslash \ identfies an escape sequence, it creates a special character.
       
-          //System.out.println("This \is an error.");                // we can only escapse certain characters (AP escape sequences are \n \" \\)                                         
+        //System.out.println("This \is an error.");                // we can only escapse certain characters (AP escape sequences are \n \" \\)                                         
       
-          System.out.println("This\nis\na\nvalid\nescape\nnsequence.\n");
-        }
-          
-      
-      
+        System.out.println("This\nis\na\nvalid\nescape\nnsequence.\n");
+    }    
 }
